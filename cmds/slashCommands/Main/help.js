b54179ebb7c4e8e09ea4c68c706c1ab9f80fa1a7
Михаил <dev_velikanov@fiftygames.ru>
2023-07-12 17:51:40 +0500

    0.1.0

--- cmds/slashCommands/Main/help.js
+++ cmds/slashCommands/Main/help.js
@@ -15,7 +15,7 @@ module.exports = {
           },
           {
             name: "Модерация",
-            value: "`ban`, `kick`, `mute`, `unmute`",
+            value: "`ban`, `kick`, `mute`, `unmute`, `channel`, `config`",
           },
           {
             name: "Музыка",
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot_id: '732867965053042690',
     color: '#255520',
-    version: '0.0.10',
+    version: '0.1.0',
     owners: ['439079453650321409'],
     data: 'Release',
     blacklist: {
