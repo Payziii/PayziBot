9458cb62acf4701adcc579d57bcadfc4a99bc7df
Михаил <dev_velikanov@fiftygames.ru>
2023-07-14 19:25:40 +0500

    l

--- index.js
+++ index.js
@@ -146,6 +146,51 @@ client.on("messageReactionAdd", async (react, user) => {
     console.log(react.count)
     console.log(user)
 })
+
+client.on("messageReactionRemove", async (react, user) => {
+    if (react.message.partial) await react.message.fetch()
+    if (react.partial) await react.fetch()
+    if(react.emoji.name != '⭐') return;
+    let guild = await Guild.findOne({ guildID: react.message.guild.id });
+    if (!guild) return;
+    if(guild.settings.starboard.channelID == '-1') return;
+    if(react.count < guild.settings.starboard.reqReacts) {
+        react.message.guild.channels.cache.get(guild.settings.starboard.channelID).messages.cache.get(msg).delete()
+        .then(message => {
+            guild.settings.starboard.data.delete(react.message.id)
+            guild.save()
+        })
+        .catch(e => console.log(e));
+        return;
+    }
+    let messageAttachment = react.message.attachments.size > 0 ? Array.from(react.message.attachments.values())[0].url : null
+
+    const embed = new EmbedBuilder()
+  .setAuthor({
+    name: react.message.author.username,
+    iconURL: react.message.author.displayAvatarURL(),
+  })
+  .setColor(guild.settings.other.color)
+  .setImage(messageAttachment);
+  let content = react.message.content.replaceAll(" ", "")
+  if(content.length > 0) embed.setDescription(react.message.content || "Пустая строка");
+  let msg = guild.settings.starboard.data.get(react.message.id);
+  if(msg == undefined) {
+    react.message.guild.channels.cache.get(guild.settings.starboard.channelID).send({content: `⭐ **${react.count}:** ${react.message.url}`, embeds: [embed] })
+    .then(message => {
+        guild.settings.starboard.data.set(react.message.id, message.id)
+        guild.save()
+    })
+    .catch(e => console.log(e));
+  }else{
+    react.message.guild.channels.cache.get(guild.settings.starboard.channelID).messages.cache.get(msg).edit({content: `⭐ **${react.count}:** ${react.message.url}`, embeds: [embed] })
+    .catch(e => console.log(e));
+  }
+
+    console.log(react.count)
+    console.log(user)
+})
+
 client.on("messageCreate", async (message) => {
     let msg = message.content;
     if (message.author.bot) return;
