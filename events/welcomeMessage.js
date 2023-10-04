40b693b6a59e583efe5cbc6dd54294c41f2fb9bb
Михаил <dev_velikanov@fiftygames.ru>
2023-10-04 10:06:27 +0500

    add welcome msg

---
+++ events/welcomeMessage.js
@@ -0,0 +1,15 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const Guild = require('../database/guild.js');
+
+module.exports = {
+	name: Events.GuildMemberAdd,
+	async execute(member, client) {
+        let guild = member.guild;
+        let g = await Guild.findOne({ guildID: guild.id });
+        if(!g) return;
+        if(g.welcome.channelID == '-1') return;
+        let channel = await client.channels.cache.get(g.welcome.channelID);
+        if(channel.guild.id != guild.id) return;
+        channel.send(g.welcome.welcomeText.replace('{user.mention}', member).replace('{user.name}', member.username).replace('{user.id}', member.id).replace('{guild.name}', guild.name))
+    }
+};
