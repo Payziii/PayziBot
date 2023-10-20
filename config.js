3f72561b832931768c191ddb445ae0d52e37a38e
Михаил <dev_velikanov@fiftygames.ru>
2023-10-20 19:03:51 +0500

    d

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     owners: ['439079453650321409', '867048292566302730'],
     tokens: {
         discord: {
-            release: 'NTc2NDQyMzUxNDI2MjA3NzQ0.GeV65R.R0P6_sBW9WwFTwL0K3qN1K9I49phKdtUpD6qXA',
+            release: 'NTc2NDQyMzUxNDI2MjA3NzQ0.GmXIkd.LxPT53QMs7Cc6PFv7cGLqM1uI7xRPqSuGEp8M4',
             beta: 'NzMyODY3OTY1MDUzMDQyNjkw.G5HoqK.H6g0KAXwGossSDMnzPZh0ByiNzifYAOdB7MgO8'
         },
         mongoURL: 'mongodb+srv://Mikhail:Mikhail2008@cluster0.b7l4v.mongodb.net/PayziBot?retryWrites=true&w=majority',
