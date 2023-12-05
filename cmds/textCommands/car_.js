0d80f70d03199184885288f4297bcb1e75128c86
Михаил <dev_velikanov@fiftygames.ru>
2023-12-05 19:10:59 +0500

    fixed

--- cmds/textCommands/car_.js
+++ cmds/textCommands/car_.js
@@ -2,39 +2,39 @@
 // И НЕ БУДЕТ ВЫПУЩЕНА В ОФИЦИАЛЬНЫЙ РЕЛИЗ PAYZIBOT
 // PAYZIBOT НЕ НЕСЕТ ОТВЕТСТВЕННОСТИ ЗА МАТЕРИАЛЫ ИЗ ДАННОЙ ИГРЫ
 
+const { EmbedBuilder } = require('discord.js');
 const cars = require('../../games_scr/custom/car_.json');
 
 exports.run = async (client, message, args, guild) => {
-	if(message.guild.id != '1149289750210019338') return;
+	if(message.guild.id != '1106931587267969036') return;
 	const item = cars[Math.floor(Math.random() * cars.length)];
+	const image = item.image[Math.floor(Math.random() * item.image.length)];
 	const collectorFilter = response => {
 		return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
 	};
 	const embed = new EmbedBuilder()
 		.setTitle('Угадай автомобиль')
 		.setDescription('У вас есть **30 секунд** чтобы ответить, какой автомобиль изображена на картинке ниже')
-		.setImage(item.image)
-		.setFooter({ text: `Игру угадали ${percent}% пользователей` })
+		.setImage(image)
 		.setColor(guild.colors.basic);
-
-	interaction.editReply({ embeds: [embed], fetchReply: true })
+	message.reply({ embeds: [embed]})
 		.then(() => {
-			interaction.channel.awaitMessages({ filter: collectorFilter, max: 1, time: 30000, errors: ['time'] })
+			message.channel.awaitMessages({ filter: collectorFilter, max: 1, time: 30000, errors: ['time'] })
 				.then(collected => {
 					const embed1 = new EmbedBuilder()
 						.setTitle('Угадай автомобиль')
 						.setDescription(`Ответ: **${item.answers[0]}**`)
-						.setImage(item.image)
+						.setImage(image)
 						.setColor(guild.colors.correct);
-					interaction.followUp({ content: `Победитель:  **${collected.first().author}**`, embeds: [embed1] });
+					message.reply({ content: `Победитель:  **${collected.first().author}**`, embeds: [embed1] });
 				})
 				.catch(() => {
 					const embed5 = new EmbedBuilder()
 						.setTitle('Угадай автомобиль')
 						.setDescription(`Ответ: **${item.answers[0]}**`)
-						.setImage(item.image)
+						.setImage(image)
 						.setColor(guild.colors.error);
-					interaction.followUp({ content: '**Победителей нет(**', embeds: [embed5] });
+					message.reply({ content: '**Победителей нет(**', embeds: [embed5] });
 				});
 		})
 };
