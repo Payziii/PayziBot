8f47dd51a7be1d80d3511412ef415bb3308d104f
Михаил <dev_velikanov@fiftygames.ru>
2023-12-14 10:57:02 +0500

    test

--- cmds/textCommands/car_.js
+++ cmds/textCommands/car_.js
@@ -6,15 +6,15 @@ const { EmbedBuilder } = require('discord.js');
 const cars = require('../../games_scr/custom/car_.json');
 
 exports.run = async (client, message, args, guild) => {
-	if(message.guild.id != '1149289750210019338') return;
+	//if(message.guild.id != '1149289750210019338') return;
 	const item = cars[Math.floor(Math.random() * cars.length)];
 	const image = item.image[Math.floor(Math.random() * item.image.length)];
 	const collectorFilter = response => {
 		return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
 	};
 	const embed = new EmbedBuilder()
-		.setTitle('Угадай автомобиль')
-		.setDescription('У вас есть **30 секунд** чтобы ответить, какой автомобиль изображена на картинке ниже')
+		.setTitle('Какое название у данного т/c?')
+		.setDescription('У вас есть **30 секунд** чтобы ответить, какое транспортное средство изображено на картинке ниже')
 		.setImage(image)
 		.setColor(guild.colors.basic);
 	message.reply({ embeds: [embed]})
@@ -22,16 +22,16 @@ exports.run = async (client, message, args, guild) => {
 			message.channel.awaitMessages({ filter: collectorFilter, max: 1, time: 30000, errors: ['time'] })
 				.then(collected => {
 					const embed1 = new EmbedBuilder()
-						.setTitle('Угадай автомобиль')
-						.setDescription(`Ответ: **${item.answers[0]}**`)
+						.setTitle('Какое название у данного т/c?')
+						.setDescription(`Ответ: **${item.answers.join(", ")}**`)
 						.setImage(image)
 						.setColor(guild.colors.correct);
 					message.reply({ content: `Победитель:  **${collected.first().author}**`, embeds: [embed1] });
 				})
 				.catch(() => {
 					const embed5 = new EmbedBuilder()
-						.setTitle('Угадай автомобиль')
-						.setDescription(`Ответ: **${item.answers[0]}**`)
+						.setTitle('Какое название у данного т/c?')
+						.setDescription(`Ответ: **${item.answers.join(", ")}**`)
 						.setImage(image)
 						.setColor(guild.colors.error);
 					message.reply({ content: '**Победителей нет(**', embeds: [embed5] });
@@ -41,8 +41,8 @@ exports.run = async (client, message, args, guild) => {
 exports.help = {
 	name: '!car',
 	aliases: [',car'],
-	info: 'owner',
+	info: 'custom command',
 	usage: '[Команда]',
-	perm: 'Developer',
-	description: 'Выполнить код',
+	perm: 'Only on a private server',
+	description: 'Guess the...',
 };
