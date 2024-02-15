473f3b90dc1980865a9fc29b533b31e713c6fe6a
Михаил <dev_velikanov@fiftygames.ru>
2024-02-15 17:52:25 +0500

    вывод сообщения за ап

--- events/ready.js
+++ events/ready.js
@@ -19,6 +19,8 @@ module.exports = {
 
 		boticord.on("notify", data => {
 			GiveReward(data.user)
+			client.channels.cache.get(channels.dbLogs)
+			.send(`${data.user} поднял бота!`)
 		})
 	},
 };
