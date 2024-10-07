3638c6455823681c5338c30eff0c49e1e4fc6f54
Payziii <velmikhail123@gmail.com>
2024-10-07 19:00:16 +0500

    Вывод в консоль информации при ошибке

--- events/ready.js
+++ events/ready.js
@@ -10,6 +10,7 @@ module.exports = {
 		console.log(`ONLINE | Bot: ${client.user.username}`);
 		client.channels.cache.get(channels.startLogs)
 			.send(`<:Bot:732119152755474444> | **${client.user.username}** запущен с **${client.guilds.cache.size}** серверами`)
+			.catch(() => console.log(`ERROR | Failed to send a startup message to the log channel`))
 
 		if(!process.env.BOTICORD) return console.log('Boticord service is not loaded. Please add boticord token in .env file')
 		
@@ -22,6 +23,7 @@ module.exports = {
 			GiveReward(data.user)
 			client.channels.cache.get(channels.dbLogs)
 			.send(`${data.user} поднял бота!`)
+			.catch(() => console.log(`ERROR | Failed to send bot boost message to log channel`))
 		})
 	},
 };
