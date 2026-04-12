585cbc1cac271ad3a3dbffb29c759d46bfb52bd2
Payziii <velmikhail123@gmail.com>
2026-04-12 21:34:08 +0500

    [ / ] Добавлено предупреждение, если команды не найдены

--- src/cmds/slashCommands/Main/help.js
+++ src/cmds/slashCommands/Main/help.js
@@ -14,27 +14,27 @@ module.exports = {
 			.addFields(
 				{
 					name: 'Утилиты',
-					value: `${Array.from(client.commands.filter(c => c.category === 'utility')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'utility')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 				{
 					name: 'Модерация',
-					value: `${Array.from(client.commands.filter(c => c.category === 'mod')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'mod')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 				{
 					name: 'Настройка',
-					value: `${Array.from(client.commands.filter(c => c.category === 'settings')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'settings')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 				{
 					name: 'Игры и профиль',
-					value: `${Array.from(client.commands.filter(c => c.category === 'games')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'games')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 				{
 					name: 'Система уровней',
-					value: `${Array.from(client.commands.filter(c => c.category === 'levels')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'levels')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 				{
 					name: 'Нейросети',
-					value: `${Array.from(client.commands.filter(c => c.category === 'neuro')).map(([key, value]) => `\`${key}\``).join(', ')}`,
+					value: `${Array.from(client.commands.filter(c => c.category === 'neuro')).map(([key, value]) => `\`${key}\``).join(', ') || "Команды не найдены"}`,
 				},
 			)
 			.setThumbnail(`https://cdn.discordapp.com/avatars/${interaction.client.user.id}/${interaction.client.user.avatar}.webp?size=4096`)
