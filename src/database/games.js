c755bf2c75bf90dd80eef8cf5de010cfe1ede6b8
Payziii <velmikhail123@gmail.com>
2026-05-11 19:52:42 +0500

    [ / ] Переименована переменная в бд

--- src/database/games.js
+++ src/database/games.js
@@ -4,7 +4,7 @@ const games = new Schema({
 	name: String,
 	id: Number,
 	all: { type: Number, default: 0 },
-	verno: { type: Number, default: 0 },
+	correct: { type: Number, default: 0 },
 });
 
 module.exports = model('Games', games);
